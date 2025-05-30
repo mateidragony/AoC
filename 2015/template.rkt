c94ac8b635632532eb072fbac2d55870dcd9a469
Cloteaux <matei.cloteaux@libertymutual.com>
2025-05-30 09:34:42 -0400

    👵 Prep 2015!

---
+++ 2015/template.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require "utils.rkt")
+
+(day 1)
+(testing #f)
+
+(for ([line lines]) (printf "~a\n" line))
+
+;; Part One                
+(printf "Part one: ~a\n" 0)
+
+;; Part Two
+(printf "Part two: ~a\n" 0)
