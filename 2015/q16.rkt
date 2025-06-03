6d5ba7525f6e7be624443ac33aac7b3465e12eb0
mateidragony <matei.cloteaux@gmail.com>
2025-06-03 12:11:21 -0400

    test

---
+++ 2015/q16.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require "utils.rkt")
+
+(day 16)
+
+(for ([line lines]) (printf "~a\n" line))
+
+;; Part One                
+(printf "Part one: ~a\n" 0)
+
+;; Part Two
+(printf "Part two: ~a\n" 0)
